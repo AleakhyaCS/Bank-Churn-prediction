1a8a98ec87d21cb2b64e0d2850b1ef4a0237118a
Aleakhya Pandeti <aleakhyapandeti2004@gmail.com>
2025-01-17 16:56:30 +0400

    ----

--- bank_churn_app.py
+++ bank_churn_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
-
+#test comment
 st.write("""
 # Bank Churn Prediction and Risk Indicator
 
